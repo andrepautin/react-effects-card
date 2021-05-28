8e95a7c83d1eeae0f413c4c31a4e17d4b1be1151
Sam Merham <sammerham-rithm@sams-mbp.lan>
2021-05-28 16:02:54 -0700

    added second effect

--- src/Deck.js
+++ src/Deck.js
@@ -11,27 +11,56 @@ import Card from "./Card";
  */
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck/";
-let deckId;
-const DRAW_CARD_URL = `${deckId}/draw/?count=1`;
+
 
 function Deck() {
+  const [deck, setDeck] = useState(null);
   const [cards, setCards] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+  console.log('rendered dec')
+
   // useEffect for API call to get new deck
   useEffect(function fetchNewDeck() {
     async function fetchDeck() {
       const deckResult = await axios.get(`${BASE_URL}/new`);
-      deckId = deckResult.data.deck_id;
-      setCards(deckResult.data);
+      setDeck(deckResult.data);
+    };
+    fetchDeck();
+  }, [setDeck]);
+  // console.log('deck', deck)
+
+  // // useEffect for API call to get a card
+  useEffect(function drawNewCard() {
+    console.log('begining of drawcard effect')
+    console.log('deck', deck)
+    async function drawCard() {
+      const cardResult = await axios.get(`${BASE_URL}${deck.deck_id}/draw?count=1`);
+      setIsDrawing(false)
+      let drawnCard= cardResult.data;
+      //when you use the callback to your set function the argument you get is your previous state
+
+      setCards(oldCards => [...oldCards, drawnCard])
+
       // setIsDrawing(false);
     }
-    fetchDeck();
-  }, []);
+    console.log('middle of drawcard effect')
+    if(isDrawing) drawCard();
+    console.log('end of drawcard effect')
+  }, [deck,isDrawing]);
+
+  function drawCard(){
+    setIsDrawing(true);
+  }
+
 
   // if remaining cards in deck > 0
     // useEffect for drawing a single card from deck
 
   return (
+    <div>
     <div>No deck yet</div>
+    <button onClick={drawCard}> Draw</button>
+    </div>
   )
 }
 
